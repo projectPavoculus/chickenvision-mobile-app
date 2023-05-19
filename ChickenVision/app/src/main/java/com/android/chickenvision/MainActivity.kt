8059a4be5cebde4ec246cc5f6ea43fc37a1f59ee
HlexNC <hlexhelftd@gmail.com>
2023-05-19 04:34:22 +0300

    Update MainActivity.kt: Request the necessary permissions

--- ChickenVision/app/src/main/java/com/android/chickenvision/MainActivity.kt
+++ ChickenVision/app/src/main/java/com/android/chickenvision/MainActivity.kt
@@ -66,6 +66,25 @@ class MainActivity : AppCompatActivity() {
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
+
     private fun takePhoto() {}
 
     private fun captureVideo() {}
